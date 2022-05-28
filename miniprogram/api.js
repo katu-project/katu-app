9c41b4ec8bc59ff0639c2fd5b0fda95b06f68c50
yxwzaxns <yxwzaxns@gmail.com>
2022-05-28 23:48:57 +0800

    区分api错误类型

--- miniprogram/api.js
+++ miniprogram/api.js
@@ -1,26 +1,35 @@
 
 const request = (action, data={}) => {
+  const error = {
+    code: 0,
+    message: ''
+  }
   return new Promise((resolve,reject)=>{
     wx.cloud.callFunction({ name: 'api', data: { action, data } })
     .then(data=>{
       if(data.result.code === 0){
         resolve(data.result.data)
       }else{
-        reject(Error(data.result.msg))
+        error.code = 1
+        error.message = data.result.msg
+        reject(error)
       }
     })
-    .catch(reject)
+    .catch(err=>{
+      error.message = err.message
+      reject(error)
+    })
 })
 }
 
 module.exports = {
   request,
-  getUser: ()=>{
-    return request('user/getUser')
-  },
-  activeAccount: data => {
-    return request('user/active', wx.cloud.CloudID(data.cloudId))
-  },
+  getUser: () => request('user/getUser'),
+
+  activeAccount: data => request('user/active', wx.cloud.CloudID(data.cloudId)),
+
+  removeAccount: () => request('user/removeAccount'),
+  
   saveCard: data=>{
     return request('user/saveCard', data)
   },
