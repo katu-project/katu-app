8978a25ee5d98d99d2d720fa811e3d54a0e179ca
aong <yxwzaxns@gmail.com>
2022-06-01 18:13:43 +0800

    增加通用设置

---
+++ miniprogram/pages/settings/general/index.js
@@ -0,0 +1,29 @@
+const { loadData, showSuccess } = require("../../../utils/index")
+const globalData = getApp().globalData
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    config_general_defaultCreateCardType: true
+  },
+  onShow(){
+    const {app:{user:{config}}} = globalData
+    this.setData({
+      config_general_defaultCreateCardType: config.general.defadefaultCreateCardType
+    })
+  },
+  tapToConfig(e){
+    const configItem = {
+      key: e.currentTarget.dataset.key,
+      value: e.detail.value
+    }
+    console.log(configItem)
+    loadData(globalData.app.updateUserConfig, configItem).then(()=>{
+      showSuccess('修改成功')
+      globalData.app.loadUserConfig(configItem)
+    })
+  }
+})
