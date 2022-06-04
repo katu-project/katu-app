5d43fd3819d7ce93d765a4f16c260e81be45dd6f
yxwzaxns <yxwzaxns@gmail.com>
2022-06-04 23:32:50 +0800

    add content edit page

---
+++ miniprogram/pages/card/edit-content/index.js
@@ -0,0 +1,26 @@
+// pages/card/edit-content/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    content: ''
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    this.returnContentKey = options.returnContentKey || 'tempData'
+  },
+  checkContent(){
+
+  },
+  tapToSetContent(){
+    const pages = getCurrentPages()
+    const prevPage = pages[pages.length-2]
+    prevPage[this.returnContentKey] = this.data.content
+    wx.navigateBack({})
+  }
+})
