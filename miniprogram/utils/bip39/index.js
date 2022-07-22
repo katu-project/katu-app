e02b40d4ba307c2155ef4f477508aace8742b56f
yxwzaxns <yxwzaxns@gmail.com>
2022-07-22 22:54:48 +0800

    添加助记词生成库bip39

---
+++ miniprogram/utils/bip39/index.js
@@ -0,0 +1,148 @@
+const crypto = require('crypto-js')
+const createHash = str => crypto.SHA256(str).toString()
+const randomBytes = len => crypto.lib.WordArray.random(len).toString()
+const Buffer = {
+  isBuffer: s => ArrayBuffer.isView(s),
+  from: s => {
+    if(Array.isArray(s)){
+      return Uint8Array.from(s)
+    }else{
+      return new Uint8Array(s.match(/../g).map(h=>parseInt(h,16)))
+    }
+  },
+  toString: byteArray => {
+    return Array.prototype.map.call(byteArray, function(byte) {
+      return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+    }).join('');
+  }
+}
+
+const DEFAULT_WORDLIST = require("./wordlists/english");
+const INVALID_MNEMONIC = 'Invalid mnemonic';
+const INVALID_ENTROPY = 'Invalid entropy';
+const INVALID_CHECKSUM = 'Invalid mnemonic checksum';
+const WORDLIST_REQUIRED = 'A wordlist is required but a default could not be found.\n' +
+    'Please pass a 2048 word array explicitly.';
+
+function normalize(str) {
+    return (str || '').normalize('NFKD');
+}
+function lpad(str, padString, length) {
+    while (str.length < length) {
+        str = padString + str;
+    }
+    return str;
+}
+function binaryToByte(bin) {
+    return parseInt(bin, 2);
+}
+function bytesToBinary(bytes) {
+    return bytes.map((x) => lpad(x.toString(2), '0', 8)).join('');
+}
+function deriveChecksumBits(entropyBuffer) {
+    const ENT = entropyBuffer.length * 8;
+    const CS = ENT / 32;
+    const hash = createHash(entropyBuffer)
+    return bytesToBinary(Array.from(hash)).slice(0, CS);
+}
+
+// exports
+function mnemonicToEntropy(mnemonic, wordlist) {
+    wordlist = wordlist || DEFAULT_WORDLIST;
+    if (!wordlist) {
+        throw new Error(WORDLIST_REQUIRED);
+    }
+    const words = normalize(mnemonic).split(' ');
+    if (words.length % 3 !== 0) {
+        throw new Error(INVALID_MNEMONIC);
+    }
+    // convert word indices to 11 bit binary strings
+    const bits = words
+        .map((word) => {
+            const index = wordlist.indexOf(word);
+            if (index === -1) {
+                throw new Error(INVALID_MNEMONIC);
+            }
+            return lpad(index.toString(2), '0', 11);
+        })
+        .join('');
+    // split the binary string into ENT/CS
+    const dividerIndex = Math.floor(bits.length / 33) * 32;
+    const entropyBits = bits.slice(0, dividerIndex);
+    const checksumBits = bits.slice(dividerIndex);
+    // calculate the checksum and compare
+    const entropyBytes = entropyBits.match(/(.{1,8})/g).map(binaryToByte);
+    if (entropyBytes.length < 16) {
+        throw new Error(INVALID_ENTROPY);
+    }
+    if (entropyBytes.length > 32) {
+        throw new Error(INVALID_ENTROPY);
+    }
+    if (entropyBytes.length % 4 !== 0) {
+        throw new Error(INVALID_ENTROPY);
+    }
+    const entropy = Buffer.from(entropyBytes);
+    const newChecksum = deriveChecksumBits(entropy);
+    if (newChecksum !== checksumBits) {
+        throw new Error(INVALID_CHECKSUM);
+    }
+    return Buffer.toString(entropy);
+}
+
+function entropyToMnemonic(entropy, wordlist) {
+    if (!Buffer.isBuffer(entropy)) {
+        entropy = Buffer.from(entropy, 'hex');
+    }
+    wordlist = wordlist || DEFAULT_WORDLIST;
+    if (!wordlist) {
+        throw new Error(WORDLIST_REQUIRED);
+    }
+    // 128 <= ENT <= 256
+    if (entropy.length < 16) {
+        throw new TypeError(INVALID_ENTROPY);
+    }
+    if (entropy.length > 32) {
+        throw new TypeError(INVALID_ENTROPY);
+    }
+    if (entropy.length % 4 !== 0) {
+        throw new TypeError(INVALID_ENTROPY);
+    }
+    const entropyBits = bytesToBinary(Array.from(entropy));
+    const checksumBits = deriveChecksumBits(entropy);
+    const bits = entropyBits + checksumBits;
+    const chunks = bits.match(/(.{1,11})/g);
+    const words = chunks.map((binary) => {
+        const index = binaryToByte(binary);
+        return wordlist[index];
+    });
+    return wordlist[0] === '\u3042\u3044\u3053\u304f\u3057\u3093' // Japanese wordlist
+        ? words.join('\u3000')
+        : words.join(' ');
+}
+
+function generateMnemonic(strength, rng, wordlist) {
+    strength = strength || 128;
+    if (strength % 32 !== 0) {
+        throw new TypeError(INVALID_ENTROPY);
+    }
+    rng = rng || randomBytes;
+    return entropyToMnemonic(rng(strength / 8), wordlist);
+}
+
+function validateMnemonic(mnemonic, wordlist) {
+    try {
+        mnemonicToEntropy(mnemonic, wordlist);
+    }
+    catch (e) {
+        return false;
+    }
+    return true;
+}
+
+module.exports = {
+    mnemonicToEntropy,
+    entropyToMnemonic,
+    generateMnemonic,
+    validateMnemonic,
+    wordlists: DEFAULT_WORDLIST
+}
