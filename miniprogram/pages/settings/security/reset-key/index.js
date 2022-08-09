a366c9b1d0ad3231f2fe3391d653ae170930e06f
aong <yxwzaxns@gmail.com>
2022-08-09 22:41:27 +0800

    add layout of reset pass page;test use ts/sass

---
+++ miniprogram/pages/settings/security/reset-key/index.js
@@ -0,0 +1,25 @@
+const { showError, loadData, showChoose, navigateTo } = require('../../../../utils/index')
+const globalData = getApp().globalData
+
+Page({
+  data: {
+    setRecoveryKey: false,
+
+  },
+  onLoad(options) {
+
+  },
+  onReady() {
+
+  },
+  onShow() {
+    const setData = {}
+    if(globalData.app.user.config.security.setRecoveryKey){
+      setData.setRecoveryKey = globalData.app.user.config.security.setRecoveryKey
+    }
+    this.setData(setData)
+  },
+  tapToPage({currentTarget:{dataset:{page}}}){
+    navigateTo(page)
+  }
+})
---
+++ miniprogram/pages/settings/security/reset-key/qrcode/index.ts
@@ -0,0 +1,17 @@
+import utils from '../../../../../utils/index'
+const globalData = getApp().globalData
+
+Page({
+  data: {
+
+  },
+  onLoad() {
+
+  },
+  onReady() {
+
+  },
+  onShow() {
+    console.log(utils,globalData.app);
+  }
+})
