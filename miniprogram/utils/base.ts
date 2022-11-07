54fc66c88dc8c731dac466190a8df10e1ca7b455
aong <yxwzaxns@gmail.com>
2022-11-07 19:47:07 +0800

    refine utils code

--- miniprogram/utils/base.ts
+++ miniprogram/utils/base.ts
@@ -4,6 +4,36 @@ export async function sleep(t=1000){
   })
 }
 
+export function selfish (target) {
+  const cache = new WeakMap();
+  const handler = {
+    get (target, key) {
+      const value = Reflect.get(target, key);
+      if (typeof value !== 'function') {
+        return value;
+      }
+      if (!cache.has(value)) {
+        cache.set(value, value.bind(target));
+      }
+      return cache.get(value);
+    }
+  };
+  const proxy = new Proxy(target, handler);
+  return proxy;
+}
+
+export function objectSetValue(obj,path,value) {
+  let i
+  path = path.split('_')
+  for (i = 0; i < path.length - 1; i++){
+    if(!(path[i] in obj)) throw Error('path not exist')
+    obj = obj[path[i]];
+  }
+  obj[path[i]] = value;
+}
+
 export default {
-  sleep
+  sleep,
+  selfish,
+  objectSetValue
 }
--- miniprogram/utils/index.ts
+++ miniprogram/utils/index.ts
@@ -1,3 +1,4 @@
+import { selfish, objectSetValue } from './base'
 import * as crypto from './crypto'
 import * as file from './file'
 import * as convert from './convert'
@@ -6,34 +7,7 @@ import { UPNG as upng } from './upng'
 import * as bip39 from './bip39/index'
 import { drawQrcode as qrcode } from './qrcode/index'
 
-export function selfish (target) {
-  const cache = new WeakMap();
-  const handler = {
-    get (target, key) {
-      const value = Reflect.get(target, key);
-      if (typeof value !== 'function') {
-        return value;
-      }
-      if (!cache.has(value)) {
-        cache.set(value, value.bind(target));
-      }
-      return cache.get(value);
-    }
-  };
-  const proxy = new Proxy(target, handler);
-  return proxy;
-}
-
-export function objectSetValue(obj,path,value) {
-  let i
-  path = path.split('_')
-  for (i = 0; i < path.length - 1; i++){
-    if(!(path[i] in obj)) throw Error('path not exist')
-    obj = obj[path[i]];
-  }
-  obj[path[i]] = value;
-}
-
+export { selfish, objectSetValue } from './base'
 export * as crypto from './crypto'
 export * as file from './file'
 export * as convert from './convert'
