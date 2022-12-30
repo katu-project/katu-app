23cf38ee31fb4c805a34d94eb56cac1e691e0be6
aong <yxwzaxns@gmail.com>
2022-12-30 13:16:48 +0800

    add contact page

---
+++ miniprogram/pages/about/contact/index.ts
@@ -0,0 +1,17 @@
+import { getAppManager } from '@/class/app'
+const app = getAppManager()
+
+Page({
+  data: {
+    email: app.Config.contacts.email
+  },
+  onLoad() {
+
+  },
+  onReady() {
+
+  },
+  onShow() {
+
+  }
+})
