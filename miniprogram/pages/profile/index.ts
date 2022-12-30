c9d1d3b5d0e74dbd246beddebccf918962d10689
aong <yxwzaxns@gmail.com>
2022-12-30 19:25:25 +0800

    refine profile page

--- miniprogram/pages/profile/index.ts
+++ miniprogram/pages/profile/index.ts
@@ -1,10 +1,11 @@
 import { loadData, navigateTo, showSuccess, showNotice, showLoading } from '@/utils/index'
-import { getAppManager } from '@/class/app'
 import { PAGES_MENU } from '@/const'
 import api from '@/api'
-const app = getAppManager()
 
-export {}
+import { getAppManager } from '@/class/app'
+import { getUserManager } from '@/class/user'
+const app = getAppManager()
+const user = getUserManager()
 
 Page({
   data: {
@@ -18,7 +19,7 @@ Page({
   },
   onReady() {
     this.setData({
-      user: app.user
+      user: user.baseInfo
     })
   },
   onShow() {
@@ -26,6 +27,7 @@ Page({
     this.checkRefreshUserData()
     this.loadCardUsageStatistic()
   },
+
   loadCardUsageStatistic(){
     api.usageStatistic().then(stats=>{
       this.setData({
@@ -34,12 +36,14 @@ Page({
       })
     })
   },
+
   tapUser(){
-    if(this.data.user.isActive) {
+    if(user.isActive) {
       return this.tapToEditInfo()
     }
     return this.showActiveNotice()
   },
+  
   async tapToActiveAccount(){
     showLoading('等待获取授权')
     wx.getUserProfile({
@@ -56,51 +60,58 @@ Page({
       }
     })
   },
+
   reloadUserInfo(){
-    app.reloadUserInfo().then(()=>{
+    user.reloadInfo().then(()=>{
       this.setData({
-        user: app.user
+        user: user.baseInfo
       })
     })
   },
 
   tapToEditInfo(){
-    if(!this.data.user.isActive) return
+    if(!user.isActive) return
     navigateTo('./edit/index')
   },
 
   checkRefreshUserData(){
-    if(app.user && (this.data.user.nickName !== app.user.nickName || this.data.user.avatarUrl !== app.user.avatarUrl)){
+    if(this.data.user.nickName !== user.baseInfo.nickName || this.data.user.avatarUrl !== user.baseInfo.avatarUrl){
       this.setData({
-        'user.avatarUrl': app.user.avatarUrl,
-        'user.nickName': app.user.nickName,
+        'user.avatarUrl': user.baseInfo.avatarUrl,
+        'user.nickName': user.baseInfo.nickName,
       })
     }
   },
+
   async tapToShowActiveTip(){
     await this.loadActiveData()
     if(this.data.activeInfo.tip){
       app.navToDoc(this.data.activeInfo.tip)
     }
   },
+
   tapToItem(e){
     const item = e.currentTarget.dataset.item
     navigateTo(item.url || item)
   },
+
   tapToReadDoc(e){
     app.navToDoc(e.currentTarget.dataset.item.id)
   },
+
   async showActiveNotice(){
     await this.loadActiveData()
     this.setData({
       showActiveNotice: true
     })
   },
+
   hideActiveNotice(){
     this.setData({
       showActiveNotice: false
     })
   },
+  
   async loadActiveData(){
     if(this.data.activeInfo.id) return
     const activeInfo = await loadData(api.getAppConfig, 'active')
