5e5dbb10db9b0c4639a56f2557e1ab1c0ce085d2
aong <yxwzaxns@gmail.com>
2023-03-06 17:29:56 +0800

    refine console.log

--- miniprogram/utils/override.ts
+++ miniprogram/utils/override.ts
@@ -12,30 +12,60 @@ console._warn = console.warn
 console._debug = console.debug
 console._error = console.error
 
+const levelColors = {
+  info: 'forestgreen',
+  warn: 'burlywood',
+  debug: 'cornflowerblue',
+  error: 'orangered'
+}
+
+const advLog = (level, ...args) => {
+  let logText:String[] = []
+  try {
+    throw Error('')
+  } catch (error) {
+    const callStack:string[] = error.stack.split('\n').slice(1)
+    for (const idx in callStack) {
+      if(!callStack[idx].includes('advLog') && callStack[idx].includes('override.js')){
+        const callInfo = /.*\/(.*?)\){0,1}$/.exec(callStack[+idx+1])
+        const callInfoText = callInfo ? callInfo[1] : ''
+        if(callInfoText) {
+          logText = [`[${time()}][${callInfoText}][%c${level}%c] `,`color:${levelColors[level]}`,'',...args]
+        }
+        break
+      }
+    }
+  }
+  if(!logText.length){
+    logText = [`[${time()}][---][%c${level}%c] `,`color:${levelColors[level]}`,'',...args]
+  }
+  console._log(...logText)
+}
+
 console.log = (...args)=>{
   if(forceDebug){
     console._log(...args)
   }else 
-  if(isDev) console._log(`[${time()}][%cinfo%c] `,'color:forestgreen','',...args)
+  if(isDev) advLog('info',...args)
 }
 
 console.warn = (...args)=>{
   if(forceDebug){
     console._warn(...args)
   }else 
-  if(isDev) console._log(`[${time()}][%cwarn%c] `,'color:burlywood','',...args)
+  if(isDev) advLog('warn',...args)
 }
 
 console.debug = (...args)=>{
   if(forceDebug){
     console._debug(...args)
   }else 
-  if(isDev) console._log(`[${time()}][%cdebug%c]`,'color:cornflowerblue','',...args)
+  if(isDev) advLog('debug',...args)
 }
 
 console.error = (...args)=>{
   if(forceDebug){
     console._error(...args)
   }else 
-  if(isDev) console._log(`[${time()}][%cerror%c]`,'color:orangered','',...args)
+  if(isDev) advLog('error',...args)
 }
