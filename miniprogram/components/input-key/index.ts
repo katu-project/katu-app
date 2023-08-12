cd1b3c26f8df90689792148367ac3205afd150cd
aong <yxwzaxns@gmail.com>
2023-08-12 21:13:20 +0800

    use new key input ui

--- miniprogram/components/input-key/index.ts
+++ miniprogram/components/input-key/index.ts
@@ -2,45 +2,150 @@ Component({
   options: {
   },
   properties: {
+    tips: {
+      type: String,
+      value: ''
+    },
     show: {
       type: Boolean,
       value: false
+    },
+    showSubBtn: {
+      type: Boolean,
+      value: false
+    },
+    advMode: {
+      type: Boolean,
+      value: false
+    },
+    subBtnText: {
+      type: String,
+      value: '忘记密码'
+    },
+    result: {
+      type: String,
+      value: ''
     }
   },
+
   data: {
     display: false,
     key: '',
-    dataLength: ''
+    focus: false, //文本框焦点
+    state: ''
   },
+
   observers:{
     'show': function(v){
-      this.setData({
-        display: v,
-        key: ''
-      })
+      if(v){
+        this.showModal(v);
+      }else{
+        this.hideModal()
+      }
+    },
+    'result': function(v){
+      if(this.data.state === '') return
+      if(v){
+        this.setData({
+          state: 'failed'
+        })
+      }
     }
   },
+
   methods: {
-    async tapToSetKey(){
-      if(!this.data.key) {
-        wx.showToast({
-          title: '输入有误',
-          icon: 'error',
-          mask: true
-        })
-        return
-      }
+    resetData(){
+      this.setData({
+        focus: false,
+        key: '',
+        state: ''
+      })
+    },
+
+    hideModal(){
       this.setData({
         display: false
       })
-      this.triggerEvent('confirm', {value: this.data.key})
+      this.resetData()
     },
-    checkKey(){},
-    hideModal(){
-      this.setData({
-        show: false,
-        key: ''
+
+    showModal(v){
+      this.setData({ 
+        display: v,
+        key: '',
+        focus: true
       })
+    },
+
+    tapSubBtn(){
+      this.hideModal()
+      this.triggerEvent('tapSubBtn')
+    },
+
+    tapToHideModal(e){
+      if(e.target.dataset.mask || e.target.dataset.btn) {
+        this.hideModal()
+        return
+      }
+    },
+
+    getFocus(){
+      this.setData({ 
+        focus: true 
+      });
+    },
+
+    advConfirm(){
+      if(!this.data.advMode) return
+      if(this.data.state === 'loading') return
+      this.setData({
+        state: 'loading'
+      })
+      setTimeout(()=>{
+        this.triggerEvent('confirm', {value: this.data.key})
+      },500)
+    },
+
+    tapChangeMode(){
+      this.setData({ 
+        advMode: !this.data.advMode
+      })
+      this.resetData()
+    },
+
+    checkKey(e){
+      const key = e.detail.value
+      if(key.length == 0){
+        this.setData({
+          state: '',
+          key: ''
+        })
+        return
+      }
+      if(this.data.advMode){
+        this.setData({
+          key: key
+        })
+      }else{
+        if(key.length > 6){
+          this.setData({
+            key: key.slice(0,6)
+          })
+        }else if(key.length === 6){
+          if(this.data.state === 'loading') return
+          this.setData({
+            state: 'loading',
+            key
+          })
+          setTimeout(()=>{
+            this.triggerEvent('confirm', {value: key})
+          },500)
+        }else{
+          this.setData({
+            key
+          })
+        }
+      }
     }
   }
 })
