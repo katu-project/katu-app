938b47bd0fdbf77781e28077951cc26435bd5fd5
aong <yxwzaxns@gmail.com>
2023-08-17 12:55:50 +0800

    change default net config

--- miniprogram/config/request.ts
+++ miniprogram/config/request.ts
@@ -1,12 +1,13 @@
 const RequestConfig:IRequestConfig = {
-  type: 'cloud',
+  type: 'http',
   cloud: {
-    env: 'dev-4gglcut52bffa0ff',
-    apiName: 'api'
+    env: '',
+    apiName: ''
   },
   http: {
-    baseUrl: '',
-    token: ''
+    baseUrl: 'https://dev.katucloud.com/api',
+    token: '' // 请访问【卡兔APP】小程序-软件设置-高级 获取开发者密钥
+              // 更多帮助文档请访问 https://dev.katucloud.com/
   }
 }
 
