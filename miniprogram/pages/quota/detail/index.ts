9b6e97f3bf26b570cd5ecfa15402f5bad92e2df6
aong <yxwzaxns@gmail.com>
2023-09-21 11:34:20 +0800

    add order detail ui

--- miniprogram/pages/quota/detail/index.ts
+++ miniprogram/pages/quota/detail/index.ts
@@ -27,9 +27,8 @@ Page({
       const createTime = new Date(log.createTime)
       log['time'] = createTime.toLocaleString('en',{hour12: false})
       if(log.type === 'coupon-exchange'){
-        log['timeName'] = '兑换时间'
+      }else if(log.type === 'quota-order'){
       }else{
-        log['timeName'] = '消耗日期'
         log['time'] = new Date(createTime.setDate(createTime.getDate()-1)).toLocaleDateString()
       }
       setData['detail'] = log
